12df9ab86d71afff43acc77d83b5795748624f99
mahnkong <mahnkong@gmx.de>
2016-11-26 19:07:10 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# About 
+This project contains a Byteman helper class and rule providing functionality to log SSL connection keys in a format used by wireshark to decrypt SSL Traffic
+
+# Build
+./gradlew build
+
---
+++ src/main/java/org/mahnkong/SSLKeyLoggerHelper.java
@@ -0,0 +1,34 @@
+package org.mahnkong;
+
+import org.jboss.byteman.rule.Rule;
+import org.jboss.byteman.rule.helper.Helper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Date;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
+
+/**
+ * Created by mahnkong on 26.11.2016.
+ */
+public class SSLKeyLoggerHelper extends Helper {
+    protected SSLKeyLoggerHelper(Rule rule) {
+        super(rule);
+    }
+
+    public void logSSLSessionData(byte[] clientNonce, byte[] masterSecret) throws IOException {
+        String logline = String.format("CLIENT_RANDOM %s %s", printHexBinary(clientNonce), printHexBinary(masterSecret));
+        String logfilePath = System.getenv("SSL_KEYLOG_PATH");
+        if (logfilePath == null || logfilePath.isEmpty()) {
+            System.out.println(logline);
+        } else {
+            Files.write(Paths.get(logfilePath), Arrays.asList(String.format("# %s", new Date().toString()), logline), UTF_8, APPEND, CREATE);
+        }
+    }
+}
